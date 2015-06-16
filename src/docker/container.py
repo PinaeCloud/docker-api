34f481e25fff7b4050a0625082cff50ce89510e5
Yugeng Hui <interhuiyg@163.com>
2015-06-17 01:08:25 +0800

    add container control method
    add container control method : start / stop / restart /  pause / unpause
    / kill / get_config / get_status / top / remove / rename

--- src/docker/container.py
+++ src/docker/container.py
@@ -1,49 +1,117 @@
 # coding=utf-8
 
 from version import check_version
+import container_config
 
 class Container():
     def __init__(self, session):
         self.session = session
  
     @check_version
-    def list(self, name = None, all = False, filters = None):
+    def list(self, name = None, all_container = False, filters = None):
         params = {
             'filter': name,
-            'all': 1 if all else 0,
+            'all': 1 if all_container else 0,
             }
         url = self.session._url('/containers/json')
         response = self.session._result(self.session._get(url, params = params))
         return response
     
     @check_version
-    def create(self, container_name, hostname, image, cmd):
+    def create(self, container_name, image, host_config, res_config, net_config, run_config):
+        #check container name
         params = {
             'name': container_name,
             }
-        config = {
-            'Hostname': hostname,
-            'Image': image,
-            'Cmd' : cmd
-            }
+        
+        config = container_config.__get_config(image, host_config, res_config, net_config, run_config)
+
         url = self.session._url('/containers/create')
         response = self.session._result(self.session._post_json(url, data=config, params=params))
         return response
     
     @check_version
-    def start(self, container_id):
-        url = self.session._url('/containers/{0}/start'.format(container_id))
-        response = self.session._result(self.session._post_json(url, data={}, params={}))
+    def rename(self, container_id, new_name):
+        if new_name:
+            params = {'name' : new_name}
+        
+        #判断容器是否重名
+        
+        url = self.session._url('/containers/{0}/rename'.format(container_id))
+        response = self.session._result(self.session._post(url, params))
+        return response
+
+    @check_version
+    def remove(self, container_id, volumes=False, force=False):
+        
+        params = {
+            'v' : volumes,
+            'force' : force 
+            }
+        
+        url = self.session._url('/containers/{0}/json'.format(container_id))
+        response = self.session._result(self.session._delete(url, params))
         return response
     
     @check_version
-    def stop(self, container_id):
+    def get_config(self, container_id):
+        url = self.session._url('/containers/{0}/json'.format(container_id))
+        response = self.session._result(self.session._get(url, params={}))
+        return response
+    
+    @check_version
+    def get_status(self, container_id):
+        url = self.session._url('/containers/{0}/stats'.format(container_id))
+        response = self.session._result(self.session._get(url, params={}))
+        return response
+
+    @check_version
+    def top(self, container_id):
+        url = self.session._url('/containers/{0}/top'.format(container_id))
+        response = self.session._result(self.session._get(url, params={}))
+        return response
+    
+    @check_version
+    def start(self, container_id):
+        url = self.session._url('/containers/{0}/start'.format(container_id))
+        response = self.session._result(self.session._post(url, params={}))
+        return response
+    
+    @check_version
+    def stop(self, container_id, wait=None):
+        if wait:
+            params = {'t' : wait}
+            
         url = self.session._url('/containers/{0}/stop'.format(container_id))
-        response = self.session._result(self.session._post_json(url, data={}, params={}))
+        response = self.session._result(self.session._post(url, params))
+        return response
+
+    @check_version
+    def restart(self, container_id, wait=None):
+        if wait:
+            params = {'t' : wait}
+            
+        url = self.session._url('/containers/{0}/stop'.format(container_id))
+        response = self.session._result(self.session._post(url, params))
+        return response
+    
+    @check_version
+    def kill(self, container_id, signal=None):
+        if signal:
+            params = {'signal' : signal}
+            
+        url = self.session._url('/containers/{0}/kill'.format(container_id))
+        response = self.session._result(self.session._post(url, params))
         return response
     
     @check_version
     def pause(self, container_id):
         url = self.session._url('/containers/{0}/pause'.format(container_id))
-        response = self.session._result(self.session._post_json(url, data={}, params={}))
+        response = self.session._result(self.session._post(url, params={}))
+        return response
+    
+    @check_version
+    def unpause(self, container_id):
+        url = self.session._url('/containers/{0}/unpause'.format(container_id))
+        response = self.session._result(self.session._post(url, params={}))
         return response
