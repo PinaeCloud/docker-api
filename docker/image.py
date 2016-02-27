9502a6ca112d61d67f5d0836504a2a0e34cbe62e
huiyugeng <interhuiyg@163.com>
2016-02-27 22:14:27 +0800

    Add history and search to image api

--- docker/image.py
+++ docker/image.py
@@ -1,15 +1,14 @@
 # coding=utf-8
 
+from text import string_utils as str_utils
+
 class Image():
     def __init__(self, session):
         self.session = session
         
-    def list(self, name = None, show_all = False, filters = None):
+    def list(self, show_all = False, filters = None):
         params = {}
-        if name != None:
-            params['filter'] = name
-        if all == True:
-            params['all'] = 1
+        params['all'] = 1 if all == True else 0
 
         url = self.session._url("/images/json")
         response = self.session._result(self.session._get(url, params = params))
@@ -17,16 +16,43 @@ class Image():
         return response
     
     def inspect(self, image_name, version = None):
+        if str_utils.is_empty(image_name):
+            raise 'Image name is Empty'
         if version != None:
             image_name = image_name + ':' + version
         url = self.session._url('/images/{0}/json'.format(image_name))
         response = self.session._result(self.session._get(url, params={}))
         return response
     
-    def history(self, image_name):
+    def history(self, image_name, version = None):
+        if str_utils.is_empty(image_name):
+            raise 'Image name is Empty'
+        if version != None:
+            image_name = image_name + ':' + version
         url = self.session._url('/images/{0}/history'.format(image_name))
         response = self.session._result(self.session._get(url, params={}))
         return response
+    
+    def search(self, image_name):
+        if str_utils.is_empty(image_name):
+            raise 'Image name is Empty'
+        params = {}
+        params['term'] = image_name
+        url = self.session._url('/images/search'.format(image_name))
+        response = self.session._result(self.session._get(url, params=params))
+        return response
+    
+    def build(self, docker_file):
+        pass
+    
+    def tag(self):
+        pass
+    
+    def import_image(self):
+        pass
+    
+    def export_image(self):
+        pass
     
     def pull(self):
         pass
@@ -34,3 +60,12 @@ class Image():
     def push(self):
         pass
     
+    def create(self):
+        pass
+    
+    def remove(self):
+        pass
+    
+
+            
+        
--- docker_test/image_test.py
+++ docker_test/image_test.py
@@ -14,9 +14,57 @@ class ImageInfoTest(unittest.TestCase):
         self.i = image.Image(self.c_session)
         
     def test_list(self):
-        image_list = self.i.list()
-        print 'list:' + json.dumps(image_list)
+        response = self.i.list()
+        status_code = response.get('status_code')
+        if status_code == 200:
+            image_list = response.get('content')
+            self.assertGreater(len(image_list), 0)
+            for image in image_list:
+                if image.get('RepoTags')[0] == 'ubuntu:14.04':
+                    print 'list: find ubuntu:14.04'
+                elif image.get('RepoTags')[0] == 'interhui/openssh:latest':
+                    print 'list: find interhui/openssh:latest'
+        if base_test.print_json:
+            print 'list:' + json.dumps(response)
         
     def test_inspect(self):
-        image_info = self.i.inspect(base_test.image_name, base_test.image_version)
-        print 'inspect:' + json.dumps(image_info)
+        response = self.i.inspect(base_test.image_name, base_test.image_version)
+        status_code = response.get('status_code')
+        if status_code == 200:
+            image_info = response.get('content')
+            self.assertEqual(image_info.get('RepoTags')[0], '{0}:{1}'.format(base_test.image_name, 
+                                                                                            base_test.image_version))
+            self.assertGreater(image_info.get('VirtualSize'), 0)
+            self.assertGreater(image_info.get('Size'), 0)
+        else:
+            self.fail('history : get image {0}:{1} fail, status_code : {2}'.format(base_test.image_name, 
+                                                                                   base_test.image_version, 
+                                                                                   status_code))
+        if base_test.print_json:
+            print 'inspect:' + json.dumps(response)
+            
+    def test_history(self):
+        response = self.i.history(base_test.image_name, base_test.image_version)
+        status_code = response.get('status_code')
+        if status_code == 200:
+            image_history = response.get('content')
+            self.assertEqual(len(image_history), 4)
+        else:
+            self.fail('history : get image {0}:{1} history fail, status_code : {2}'.format(base_test.image_name, 
+                                                                                           base_test.image_version, 
+                                                                                           status_code))
+        if base_test.print_json:
+            print 'history:' + json.dumps(response)
+            
+    def test_search(self):
+        response = self.i.search('interhui/mysql')
+        status_code = response.get('status_code')
+        if status_code == 200:
+            image_list = response.get('content')
+            self.assertEqual(len(image_list), 2)
+        else:
+            self.fail('search : search image {0} fail, status_code : {1}'.format('interhui/mysql', 
+                                                                                 status_code))
+        if base_test.print_json:
+            print 'search:' + json.dumps(response)
+        
