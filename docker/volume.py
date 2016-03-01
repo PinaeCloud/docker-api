e55de50d836c6a9e86b94fa6ad5393e61f0da9be
huiyugeng <interhuiyg@163.com>
2016-03-01 21:15:32 +0800

    Add volume api

--- docker/volume.py
+++ docker/volume.py
@@ -1,6 +1,31 @@
 # coding=utf-8
 
-class Volumn():
+from text import string_utils as str_utils
+
+class Volume():
     
-    def __init__(self):
-        pass
+    def __init__(self, session):
+        self.session = session
+    
+    def list(self, filters = None):
+        params = {'filters': filters}
+        url = self.session._url('/volumes')
+        return self.session._result(self.session._get(url, params = params))
+    
+    def inspect(self, volume_name):
+        url = self.session._url('/volumes/{0}'.format(volume_name))
+        return self.session._result(self.session._get(url))
+    
+    def create(self, volume_name, driver = None, driver_opts = None):
+        if str_utils.is_not_empty(volume_name):
+            raise IOError('Volume name is Empty') 
+        if driver_opts is not None and not isinstance(driver_opts, dict):
+            raise TypeError('driver_opts must be a dictionary')
+
+        req_data = {'Name': volume_name, 'Driver': driver, 'DriverOpts': driver_opts}
+        url = self.session._url('/volumes/create')
+        return self.session._result(self.session._post_json(url, data = req_data), True)
+    
+    def remove(self, volume_name):
+        url = self.session._url('/volumes/{0}'.format(volume_name))
+        return self.session._result(self.session._delete(url))
