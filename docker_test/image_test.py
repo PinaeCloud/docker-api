43ba66ba2a4d85bac2d4141001f6b01f67b68a7a
huiyugeng <interhuiyg@163.com>
2016-03-01 15:31:28 +0800

    Update images test case

--- docker_test/image_test.py
+++ docker_test/image_test.py
@@ -5,7 +5,6 @@ import json
 from docker import session
 from docker import image
 
-from docker.utils import auth
 from docker_test import base_test
 
 class ImageInfoTest(unittest.TestCase):
@@ -25,6 +24,9 @@ class ImageInfoTest(unittest.TestCase):
                     print 'list: find ubuntu:14.04'
                 elif image.get('RepoTags')[0] == 'interhui/openssh:latest':
                     print 'list: find interhui/openssh:latest'
+        else:
+            self.fail('list: list images fail, status_code : ' + status_code)
+            
         if base_test.print_json:
             print 'list:' + json.dumps(response)
         
@@ -38,7 +40,7 @@ class ImageInfoTest(unittest.TestCase):
             self.assertGreater(image_info.get('VirtualSize'), 0)
             self.assertGreater(image_info.get('Size'), 0)
         else:
-            self.fail('history : get image {0}:{1} fail, status_code : {2}'.format(base_test.image_name, 
+            self.fail('inspect : get image {0}:{1} fail, status_code : {2}'.format(base_test.image_name, 
                                                                                    base_test.image_version, 
                                                                                    status_code))
         if base_test.print_json:
@@ -79,5 +81,5 @@ class ImageInfoTest(unittest.TestCase):
             image_info = resp_info.get('content')
             self.assertEqual(image_info.get('RepoTags')[0], '{0}:{1}'.format('busybox', 'latest'))
         else:
-            self.fail('Fail pull image : busybox:latest')
+            self.fail('pull: pull image fail: busybox:latest, status_code : ' + status_code)
         
